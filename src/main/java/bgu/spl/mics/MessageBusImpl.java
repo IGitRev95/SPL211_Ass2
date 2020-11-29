16f9af1b1b2058c9d9dd4f0518b81aadbc27df41
IGitRev95 <idorev@post.bgu.ac.il>
2020-11-29 22:53:15 +0200

    Revert "yalla"
    This reverts commit 2a4fea61

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,4 +1,6 @@
 package bgu.spl.mics;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
@@ -6,48 +8,63 @@ package bgu.spl.mics;
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
-	
-	
+
+
+	private ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Message>> MessegeQueses= new ConcurrentHashMap<>();
+
+	private static class SingletonHolder {
+		private static MessageBusImpl instance= new MessageBusImpl();
+	}
+
+	public static MessageBusImpl getInstance() {
+		return SingletonHolder.instance;
+	}
+
+
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		
+
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		
-    }
 
-	@Override @SuppressWarnings("unchecked")
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
-		
+
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		
+
 	}
 
-	
+
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		
-        return null;
+
+		return null;
 	}
 
 	@Override
 	public void register(MicroService m) {
-		
+
 	}
 
 	@Override
 	public void unregister(MicroService m) {
-		
+		// will not be checked
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		
+
 		return null;
 	}
+
+
+
 }
