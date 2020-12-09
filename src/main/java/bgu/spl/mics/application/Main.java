7575e932b3e0ca0666f5e379dc4b8f23b3defe41
IGitRev95 <idorev@post.bgu.ac.il>
2020-12-09 23:43:10 +0200

    Main Done

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -17,24 +17,31 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		threadInitCounter = new CountDownLatch(4);
+
 		//Json Parsing
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		ParseJson inputJson = gson.fromJson(new FileReader(args[0]),ParseJson.class);
+		ParseJson inputJson = gson.fromJson(new FileReader( args[0] ), ParseJson.class);
 
-		Ewoks.init(inputJson.Ewoks);
-		LeiaMicroservice pLeia = new LeiaMicroservice(inputJson.attacks);
-		R2D2Microservice r2D2 = new R2D2Microservice(inputJson.R2D2);
-		LandoMicroservice lando = new LandoMicroservice(inputJson.Lando);
+		//init objects with input data json related
+		Ewoks.init( inputJson.Ewoks );
+		LeiaMicroservice pLeia = new LeiaMicroservice( inputJson.attacks );
+		R2D2Microservice r2D2 = new R2D2Microservice( inputJson.R2D2 );
+		LandoMicroservice lando = new LandoMicroservice( inputJson.Lando );
 
 		HanSoloMicroservice hSolo = new HanSoloMicroservice();
 		C3POMicroservice c3po = new C3POMicroservice();
 
+		//Threads init
 		Thread[] threads ={new Thread(lando),new Thread(r2D2),new Thread(hSolo),new Thread(c3po)};
 		for (Thread th:threads ) { th.start();}
+
+		//Leia waiting for all other micro services to initialize
 		try {
 			threadInitCounter.await();
 			Thread pL = new Thread(pLeia);
 			pL.start();
+
+			//waiting for all threads to terminate
 			pL.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -48,15 +55,15 @@ public class Main {
 			}
 		}
 
-
+		//Generating Output Json
 		try {
 			objectToOutputJsonFile(gson, Diary.getInstance() , args[1]);
-		}catch (IOException exp){ exp.printStackTrace();}
+		}catch (IOException exp){ exp.printStackTrace(); }
 
-		System.out.println("Finish");
+		System.out.println("To Be Continue... The End?");
 	}
 
-	/** This is a class that provide a conviniant way
+	/** This is a class that provide a convenient way
 	 * accessing the input data parsed from the input json file
 	 */
 	public class ParseJson{
@@ -66,15 +73,14 @@ public class Main {
 		private int Ewoks=0;
 	}
 
-
 	/** This method creates an output json file from an object at defined path given by user
 	 * @param gson - Gson parser
 	 * @param toOutputFile - Object to converted to json file
 	 * @param outPutPath - output json file path
 	 */
 	public static void objectToOutputJsonFile(Gson gson, Object toOutputFile,String outPutPath) throws IOException {
-		FileWriter outputJson = new FileWriter(new File(outPutPath+"/output.json"));//TODO: Before submission remove "+"/output.json""
-		gson.toJson(toOutputFile,outputJson);
+		FileWriter outputJson = new FileWriter(new File( outPutPath ));
+		gson.toJson( toOutputFile, outputJson);
 		outputJson.close();
 	}
 }
